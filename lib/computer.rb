a8b9203ca3b471b7c9feef700e614174ecdd93c7
JD Alegre Casao <117078422+dyandel-dev@users.noreply.github.com>
2026-01-05 12:45:26 +0800

    Add same_color? and color_exist? method in Computer Class

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,16 +1,28 @@
 class Computer
   COLORS = %w[red blue green yellow orange purple].freeze
 
-  attr_accessor :code
+  attr_accessor :code, :feedback
 
   def initialize
     @code = Hash.new(4)
+    @feedback = Hash.new(4)
+  end
+
+  def create_key(index)
+    "code#{index + 1}".to_sym
   end
 
   def generate_code
     4.times do |index|
-      key = "code#{index + 1}".to_sym
-      code[key] = COLORS.sample
+      code[create_key(index)] = COLORS.sample
     end
+  end
+
+  def same_color?(index, player_guess)
+    code[create_key(index)] == player_guess[create_key(index)]
+  end
+
+  def color_exist?(index, player_guess)
+    code.value?(player_guess[create_key(index)])
   end
 end
